d9629da08a5936b24986af6d253520e08acf78bb
Rifkyain <90232550+Rifkyain@users.noreply.github.com>
2022-05-11 10:26:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var x = "Haloo";
+console.log("hay");
+console.log("sdsdad");
+var n=prompt("Masukkan Nama:");
+if (n==="xxx"){
+	alert(x + " " + "xxx ^_^");
+}else {
+	alert("enakan dipanggil xxx")
+}
